f403911719feef59ade472e77be51b9b145c878a
Antoine Chassagne <antoine.chassagne@me.com>
2021-01-08 15:02:21 +0100

    Clean data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState, useEffect } from "react";
+import * as d3 from "d3";
+import csvFile from "./data/military_expenditure.csv";
+
 function App() {
-  return (
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    d3.csv(csvFile).then(rawData => {
+      setData(rawData.map(item => {
+        const { Code: code, Name: label, Type, "Indicator Name": indicatorName, ...dates } = item;
+        return {
+          code: item.Code,
+          label: item.Name,
+          values: Object.keys(dates).map(key => ({
+            date: key,
+            value: parseFloat(dates[key] || 0)
+          }))
+        }
+      }));
+    });
+  }, []);
+
+  return (  
     <div className="App">
       Foo
     </div>
