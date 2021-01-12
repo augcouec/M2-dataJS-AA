fb003bd68365260ac880da0abcccfa7cfbe57820
Antoine Chassagne <antoine.chassagne@me.com>
2021-01-12 21:59:21 +0100

    Add data formating, computation and update based on dates range

--- src/App.js
+++ src/App.js
@@ -1,40 +1,84 @@
 import { useState, useEffect } from "react";
 import * as d3 from "d3";
 import csvFile from "./data/military_expenditure.csv";
-import { FIRST_YEAR, LAST_YEAR } from "./constants";
 import DatesRangeSlider from "./components/DatesRangeSlider";
+
+const formatData = (rawData) => {
+  return rawData.map((item) => {
+    const {
+      Code: code,
+      Name: label,
+      Type,
+      "Indicator Name": indicatorName,
+      ...dates
+    } = item;
+
+    const country = { code, label };
+
+    country.expenses = Object.keys(dates).map((key) => ({
+      date: parseFloat(key),
+      value: parseFloat(dates[key] || 0),
+    }));
+
+    return country;
+  });
+};
+
+const computeData = (data) => {
+  return data.map((country) => {
+    country.average = country.expenses.reduce(
+      (total, expense, currentIndex, expenses) => {
+        total = total + expense.value;
+        if (expenses.length === currentIndex + 1) {
+          if (total === 0) return total;
+          const count = expenses.filter((exp) => exp.value > 0).length;
+          return total / count;
+        }
+        return total;
+      },
+      0
+    );
+
+    country.total = country.expenses.reduce((total, expense) => {
+      total = total + expense.value;
+      return total;
+    }, 0);
+
+    return country;
+  });
+};
+
+const updateDataRange = (data, datesRange) => {
+  return data.map((country) => {
+    const expenses = country.expenses.filter(
+      (expense) => expense.date > datesRange[0] && expense.date < datesRange[1]
+    );
+    return { ...country, expenses };
+  });
+};
 
 function App() {
   const [data, setData] = useState([]);
-  const [datesRange, setDatesRange] = useState([FIRST_YEAR, LAST_YEAR]);
+  const [currentData, setCurrentData] = useState([]);
 
   useEffect(() => {
-    d3.csv(csvFile).then((rawData) => {
-      setData(
-        rawData.map((item) => {
-          const {
-            Code: code,
-            Name: label,
-            Type,
-            "Indicator Name": indicatorName,
-            ...dates
-          } = item;
-          return {
-            code: item.Code,
-            label: item.Name,
-            values: Object.keys(dates).map((key) => ({
-              date: key,
-              value: parseFloat(dates[key] || 0),
-            })),
-          };
-        })
-      );
+    d3.csv(csvFile).then((csv) => {
+      const formattedData = formatData(csv);
+      const computedData = computeData(formattedData);
+      setData(computedData);
+      setCurrentData(computedData);
     });
   }, []);
 
+  const updateDatesRange = (datesRange) => {
+    const updatedData = updateDataRange(data, datesRange);
+    const computedData = computeData(updatedData);
+    setCurrentData(computedData);
+  };
+
   return (
     <div className="App">
-      <DatesRangeSlider updater={setDatesRange} />
+      <DatesRangeSlider updater={updateDatesRange} />
     </div>
   );
 }
