1f1620f5ec6fb77d3a87a1ba4fcd2827e0f385a5
Antoine Chassagne <antoine.chassagne@me.com>
2021-01-15 11:28:32 +0100

    Add legends on line chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -33,7 +33,7 @@ function LineChart(props) {
           <ResponsiveLine
             data={data}
             colors={(d) => d.color}
-            margin={{ top: 30, right: 40, bottom: 60, left: 80 }}
+            margin={{ top: 30, right: 160, bottom: 60, left: 80 }}
             xScale={{ type: "point" }}
             yScale={{
               type: "linear",
@@ -55,6 +55,32 @@ function LineChart(props) {
               tickRotation: 0,
             }}
             useMesh={true}
+            legends={[
+              {
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 100,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
           />
         </>
       ) : (
